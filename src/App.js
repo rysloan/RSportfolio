f094376514be523d24c24c07b0d5d00b7a6b68d1
u1261748@eng.utah.edu <u1261748@eng.utah.edu>
2024-05-22 12:03:54 -0600

    fixed routes

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import Skills from "./components/Skills/skills"
 import Works from "./components/Works/works"
 import Contact from "./components/Contact/contact"
 import Footer from "./components/Footer/footer"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Ivorfall from "./pages/Ivorfall"
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
             <Navbar />
             <Intro />
             <Skills />
@@ -20,13 +20,13 @@ function App() {
             <Contact />
             <Footer />
           </Route>
-          <Route exact path="/Ivorfall">
+          <Route path="/Ivorfall">
             <Navbar />
             <Ivorfall />
             <Footer />
           </Route>
-        </Switch>
-      </Router>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
