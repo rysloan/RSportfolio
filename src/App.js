676f69cd4701951b6813643e70c75ab4df110a87
u1261748@eng.utah.edu <u1261748@eng.utah.edu>
2024-06-19 10:45:27 -0700

    added hashrouter

--- src/App.js
+++ src/App.js
@@ -4,25 +4,25 @@ import Skills from "./components/Skills/skills"
 import Works from "./components/Works/works"
 import Contact from "./components/Contact/contact"
 import Footer from "./components/Footer/footer"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Ivorfall from "./pages/Ivorfall"
 import Main from "./pages/Main"
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      {/* <Navbar />
       <Intro />
       <Skills />
       <Works />
       <Contact />
-      <Footer />
-      {/* <BrowserRouter>
+      <Footer /> */}
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/Ivorfall" element={<Ivorfall />} />
         </Routes>
-      </BrowserRouter> */}
+      </HashRouter>
     </div>
   );
 }
